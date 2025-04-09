0fd13fbec95433d121c2eeb832109c86f708be2c
dodoyoo <dong15903@daum.net>
2025-04-09 15:21:29 +0900

    FEAT: Connect GitHub

---
+++ src/main/java/com/croom/CroomApplication.java
@@ -0,0 +1,13 @@
+package com.croom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CroomApplication.class, args);
+	}
+
+}
